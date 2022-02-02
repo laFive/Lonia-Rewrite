bf9afc472f5877c6321e8d6684c2fe8c4a5b455d
laFive <79715115+laFive@users.noreply.github.com>
2022-02-02 11:21:53 +0000

    Update KillAuraE.java
    Start check

--- src/me/five/lonia/check/impl/killaura/KillAuraE.java
+++ src/me/five/lonia/check/impl/killaura/KillAuraE.java
@@ -1,1 +1,33 @@
 package me.five.lonia.checks.impl.killaura;
+
+import me.five.lonia.checks.Check;
+import me.five.lonia.packet.LoniaPacket;
+import me.five.lonia.packet.client.CPacketFlying;
+import me.five.lonia.packet.client.CPacketUseEntity;
+
+public class KillAuraE extends Check {
+  
+  private boolean flagged;
+  private long lastFlying;
+  
+  public KillAuraE() {
+    super("KillAura", "E", 0, 8, true);
+  }
+  
+  @Override
+  public void handle(LoniaPacket packet) {
+   
+    if (packet instanceof CPacketFlying) {
+     
+      long lastFlying = this.lastFlying;
+      this.lastFlying = System.currentTimeMillis();
+      if (flagged && lastFlying - System.currentTimeMillis() > 45L) {
+        flag(1, "FlyingDelay:" + System.currentTimeMillis() - lastFlying;
+      }
+      flagged = false;
+      
+    }
+    
+  }
+  
+}
